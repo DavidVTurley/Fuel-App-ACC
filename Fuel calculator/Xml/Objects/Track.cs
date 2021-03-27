4adf1151f4871f6631c4df46287318e09d816ccf
David Turley <48710656+DavidVTurley@users.noreply.github.com>
2021-03-27 19:08:38 +0100

    Added Xml definitions

--- Fuel calculator/Xml/Objects/Track.cs
+++ Fuel calculator/Xml/Objects/Track.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,19 @@ using System.Xml.Serialization;
 namespace Fuel_calculator.Xml.Objects
 {
     [XmlRoot("Track")]
-    class Track
+    public class Track
     {
-        [XmlElement("Name")] public Int32 Name;
+        [XmlElement("Name")] public String Name;
+    }
+
+    public class Tracks
+    {
+        [XmlElement("Track")] public List<Track> Track;
+
+
+        public static Tracks LoadTracksFromXml()
+        {
+            return Xml_deserializer.Xml.Deserialize<Tracks>(Directory.GetCurrentDirectory() + "\\Xml\\Tracks.xml");
+        }
     }
 }
--- Fuel calculator/Xml/Objects/TrackCarCombo.cs
+++ Fuel calculator/Xml/Objects/TrackCarCombo.cs
@@ -1,18 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Documents;
 using System.Xml.Serialization;
 
 namespace Fuel_calculator.Xml.Objects
 {
-    public class TrackCarCombo
+    [XmlRoot("Car_Track_Combo")]
+    public class CarTrackCombo
     {
-        [XmlRoot("Car_Track_Combo")]
-        class CarTrackCombo
-        {
-            [XmlElement("Track")] public Track Track;
-            [XmlElement("Car")] public Car Car;
+        [XmlElement("Track")] public Track Track;
+        [XmlElement("Car")] public Car Car;
+    }
 
-            [XmlElement("Avarage_Lap_Time")] public Car AverageLapTime;
-            [XmlElement("Fuel_Per_Lap")] public Car FuelPerLap;
+    [XmlRoot("SavedCombinations")]
+    public class SavedCombinations
+    {
+        [XmlElement("CarTrackCombo")] public List<CarTrackCombo> CarTrackCombo;
+        [XmlElement("FuelPerLap")] public Single FuelPerLap;
+
+
+        public static SavedCombinations LoadCarTrackComboFromXml()
+        {
+            return Xml_deserializer.Xml.Deserialize<SavedCombinations>(Directory.GetCurrentDirectory() + "\\Xml\\CarTrackCombo.xml");
         }
     }
 }
